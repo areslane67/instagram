d7c32d8ba9399cfa2405a9b0cfdcba9285383457
Areslane <81231071+areslane67@users.noreply.github.com>
2021-12-18 20:26:25 +0100

    first commit

---
+++ js/acceuil.js
@@ -0,0 +1,3 @@
+let submit_log = localStorage.getItem("name");
+    yesmail.innerText += ` ${submit_log}`;
+    yesmail2.innerText += ` ${submit_log}`;
---
+++ js/index.js
@@ -0,0 +1,26 @@
+var name = document.getElementById('mail');
+var pw = document.getElementById('mdp');
+
+
+localStorage.setItem('name', 'areslane67@gmail.com');
+localStorage.setItem('pw', 'sa merite un 20');
+
+
+
+function check() {
+
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+
+    var mail = document.getElementById('mail');
+    var mdp = document.getElementById('mdp');
+
+   
+    if(mail.value !== storedName && mdp.value !== storedPw) {
+        error.textContent = 'Mail ou mot de passe incorrecte';
+    }else if(mail.value == storedName && mdp.value !== storedPw){
+        error.textContent = 'Mot de passe incorrecte';
+    }else{
+        lien.href ="acceuil.html";
+    }
+}
